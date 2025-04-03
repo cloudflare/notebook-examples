24c2565912d8b2d0bde143369553d672e165980a
Jorge Pacheco <jpacheco@cloudflare.com>
2025-04-03 16:17:07 +0100

    Remove write operations from D1 notebook

--- notebooks/d1.py
+++ notebooks/d1.py
@@ -15,16 +15,6 @@ def _():
     return alt, json, mo, np, pd, requests
 
 
-app._unparsable_cell(
-    r"""
-    TODO:
-     - separate warnings as markdown
-     - explain how to delete created files and buckets
-    """,
-    name="_"
-)
-
-
 @app.cell
 def _(mo):
     mo.md(
@@ -33,15 +23,15 @@ def _(mo):
 
         In this notebook, we will show a simple use case involving interactions with D1 associated to a
         Cloudflare account. This will involve the following:<br>
-         1. creating a D1 dataset;<br>
-         2. listing existing D1 datasets;<br>
-         3. simple query listing tables of a D1 dataset.<br>
+         1. listing existing D1 datasets;<br>
+         2. simple query fetching rows from a D1 dataset table;<br>
 
         all using the Cloudflare API.
 
         **Prerequisites:**<br>
          - API token (see [here](https://developers.cloudflare.com/r2/api/s3/tokens/)
-        for info on how to create one) with **D1 write** permission.
+        for info on how to create one) with **D1 read** permission;<br>
+         - at least one D1 dataset with a table.
         """
     )
     return
@@ -49,8 +39,8 @@ def _(mo):
 
 @app.cell
 def _():
-    CF_ACCOUNT_TAG = 'your-account-tag'
-    TOKEN = "your-token"
+    CF_ACCOUNT_TAG = '<your-account-tag>'
+    TOKEN = '<your-token>'
 
     HOSTNAME = 'https://examples-api-proxy.notebooks.cloudflare.com'
     URL = f'{HOSTNAME}/client/v4/accounts/{CF_ACCOUNT_TAG}/d1/database'
@@ -61,54 +51,9 @@ def _():
 def _(mo):
     mo.md(
         r"""
-        ## Create new D1 dataset
-
-        We will start by creating a new dataset.
-        For this, we will make use of the `/accounts/{account_id}/d1/database`
-        [endpoint](https://developers.cloudflare.com/api/resources/d1/subresources/database/methods/create/).
-        This endpoint will return all account D1 datasets by default.
-        A POST request with the name of the dataset in the payload will create a new entry.
-        """
-    )
-    return
-
-
-@app.cell
-def _():
-    NEW_DATASET_NAME = 'test-dataset'
-    return (NEW_DATASET_NAME,)
-
-
-@app.cell
-def _(NEW_DATASET_NAME, TOKEN, URL, requests):
-    # Warning: skip this cell if the dataset already exists
-
-    # Send POST query with dataset name as payload
-    _payload = dict(name=NEW_DATASET_NAME)
-    new_dataset_resp = requests.post(URL, headers={'Authorization': 'Bearer {}'.format(TOKEN)},
-                                     json=_payload)
-
-    # Results handling
-    new_dataset_resp_json = new_dataset_resp.json()
-    if new_dataset_resp_json['success']:
-        _new_dataset_info = new_dataset_resp_json['result']
-        print(f"Successfully created new D1 dataset: {_new_dataset_info['name']} ({_new_dataset_info['uuid']})")
-    else:
-        print(f'Failed to create new D1 dataset (status code {new_dataset_resp.status_code}).',
-              'Received the following errors:')
-        for _error in new_dataset_resp_json['errors']:
-            print(f" - {_error['code']}: {_error['message']}")
-        new_dataset_resp.raise_for_status()
-    return new_dataset_resp, new_dataset_resp_json
-
-
-@app.cell
-def _(mo):
-    mo.md(
-        r"""
         ## List available D1 datasets
 
-        By performing a GET request (with no payload) to the same `/accounts/{account_id}/d1/database`
+        By performing a GET request (with no payload) to the `/accounts/{account_id}/d1/database`
         [endpoint](https://developers.cloudflare.com/api/resources/d1/subresources/database/methods/list/)
         we can get the names, unique Ids as well as other info of all D1 datasets associated with a Cloudflare account.
         """
@@ -137,6 +82,19 @@ def _(TOKEN, URL, pd, requests):
 @app.cell
 def _(d1_datasets):
     d1_datasets
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        /// admonition |
+
+        The same endpoint can also be used for creating a new D1 dataset. This can be done by performing a POST
+        request with the name of the dataset as the payload.
+        """
+    )
     return
 
 
@@ -178,109 +136,64 @@ def _(HOSTNAME, requests):
 
 
 @app.cell
-def _():
-    DATABASE_ID = 'your-database-id'
-    return (DATABASE_ID,)
-
-
-@app.cell
-def _(CF_ACCOUNT_TAG, DATABASE_ID, TOKEN, query_d1):
-    # Note: D1 uses SQLite syntax
-    _query = '''
-    CREATE TABLE IF NOT EXISTS simple(
-        t       INTEGER PRIMARY KEY AUTOINCREMENT,
-        val     INTEGER
-    );
-    '''
-
-    _ = query_d1(
-        CF_ACCOUNT_TAG,
-        DATABASE_ID,
-        TOKEN,
-        _query
-    )
-    return
-
-
-@app.cell
 def _(mo):
     mo.md(
         r"""
-        For the purposes of this demonstration, we will create a short (slightly noisy) sinusoidal function
-        which will be stored in the previously created table.
-        """
-    )
-    return
+        /// admonition | Setting up D1 datasets
 
-
-@app.cell
-def _(CF_ACCOUNT_TAG, DATABASE_ID, TOKEN, np, query_d1):
-    # Warning: rerunning this cell will append new data to an already existing table!
-
-    # Number of entries
-    _SIZE_TS = 100
-
-    # Create sinusoidal time series with fixed seed
-    _rng = np.random.default_rng(1234)
-    _ts = np.cos(np.arange(_SIZE_TS)) + _rng.random(_SIZE_TS)
-
-    # Generate the SQL INSERT query based on the generated time series
-    _query_inserts = ',\n'.join([f'({float(el)})' for el in _ts])
-    _query = f'''
-    INSERT INTO simple(val) VALUES
-    {_query_inserts};
-    '''
-
-    _ = query_d1(
-        CF_ACCOUNT_TAG,
-        DATABASE_ID,
-        TOKEN,
-        _query
-    )
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md(
-        r"""
-        This is a more manual approach to populating SQL tables. The alternative would be to import an already existing
-        database into D1, although that approach requires efforts outside of this notebook.
+        While not shown in this notebook, SQL tables can be created and populated on the API side by running queries
+        with `CREATE TABLE` and `INSERT INTO` respectively. Both of these will require tokens with `write`
+        permissions, however, and represent a more manual approach to populating SQL tables. The alternative would be
+        to import an already existing database into D1.
 
         More information on how to import (and export) data from D1 datasets can be found in
         [this dev page](https://developers.cloudflare.com/d1/best-practices/import-export-data/).
 
-        Now, let's test the table by querying its contents and visualising the time series.
+        ///
+
+        Queries vary from table to table, so we will perform a simple fetch of the first 10 lines of a given table.
         """
     )
     return
 
 
 @app.cell
-def _(CF_ACCOUNT_TAG, DATABASE_ID, TOKEN, query_d1):
-    # Obtain previously created time series
-    _query = 'SELECT * FROM simple;'
+def _():
+    # The database being queried (examples are the uuid in the d1_datasets dataframe above)
+    DATABASE_ID = '<your-database-id>'
 
-    ts_resp = query_d1(
+    TABLE_NAME = '<your-database-table>'
+    return DATABASE_ID, TABLE_NAME
+
+
+@app.cell
+def _(CF_ACCOUNT_TAG, DATABASE_ID, TABLE_NAME, TOKEN, query_d1):
+    _query = f'SELECT * FROM {TABLE_NAME} LIMIT 10;'
+
+    query_resp_raw = query_d1(
         CF_ACCOUNT_TAG,
         DATABASE_ID,
         TOKEN,
         _query
     )
-    return (ts_resp,)
+    return (query_resp_raw,)
 
 
 @app.cell
-def _(alt, pd, ts_resp):
-    ts_df = pd.DataFrame(ts_resp[0]['results'])
+def _(pd, query_resp_raw):
+    try:
+        query_output = pd.DataFrame(query_resp_raw[0]['results'])
+    except Exception as e:
+        print('Could not parse results into a dataframe:', e)
+        print('Will display raw results instead')
+        query_output = query_resp_raw
+    return (query_output,)
 
-    alt.Chart(ts_df, title='Sinusoidal time series from D1').mark_line().encode(
-        alt.X('t',
-              title='Index'),
-        alt.Y('val',
-              title='Value')
-    ).properties(height=400, width=500)
-    return (ts_df,)
+
+@app.cell
+def _(query_output):
+    query_output
+    return
 
 
 if __name__ == "__main__":
